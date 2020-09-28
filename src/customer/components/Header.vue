07f59992e0ecde079679b39aba58a17c138f6e0f
Phuong Pham <phuongpham98ptit@gmail.com>
2020-09-28 18:20:36 +0700

    folder

---
+++ src/customer/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <header class="site-header" role="banner">
+    <div class="site-header">
+      <div class="content">
+        <div class="clearfix " role="article">
+          <div class="content">
+          <a href="#site-content" class="visuallyhidden">Skip navigation to main content</a>
+            <div class="site-header__container js-site-header--v1 js-site-header">
+              <div class="site-header__main">
+                <div class="site-header__nav">
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "NavBar"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import router from './router/router'
+import './assets/index.scss'
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
