17afba1bed630b6c20652a5ed7d9c617e0f8b9c8
Phuong Pham <phuongpham98ptit@gmail.com>
2020-10-23 19:09:31 +0700

    jfjg

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router/router'
-import './assets/index.scss'
-Vue.config.productionTip = false
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router/router";
+import "./customer/assets/index.scss";
+import "./customer/assets/sass/style.scss";
+Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
